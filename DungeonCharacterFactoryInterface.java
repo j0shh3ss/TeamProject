247f16b60f17a57c5807b4d1982811b83f62bc3b
Ian-Goss <84542196+Ian-Goss@users.noreply.github.com>
2021-06-02 14:43:03 -0700

    Update DungeonCharacterFactoryInterface.java

--- DungeonCharacterFactoryInterface.java
+++ DungeonCharacterFactoryInterface.java
@@ -1,13 +1,21 @@
 public class DungeonCharacterFactoryInterface {
     protected String name;
     protected int hitPoints;
-    
+    protected int attackSpeed;
+    protected double chanceToHit;
+    protected int damageMin;
+    protected int damageMax;
     //protected stuff because i want to encapsolate it
 
-    public DungeonCharacterStats(String name, int hitPoints)
+    public DungeonCharacterStats(String name, int hitPoints, int attackSpeed, int damageMin, int damageMax, double chanceToHit)
     {
 
         this.name = name;
         this.hitPoints = hitPoints;
+        this.attackSpeed = attackSpeed;
+        this.damageMin = damageMin;
+        this.damageMax = damageMax;
+        this.chanceToHit = chanceToHit;
+ 
     }
 }
