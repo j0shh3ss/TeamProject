bca448516d0d65277ae5252b73e95501de9fbed2
Burgen-Hintz <84937856+Burgen-Hintz@users.noreply.github.com>
2021-06-02 15:26:29 -0700

    Add files via upload

---
+++ MonsterInterface.java
@@ -0,0 +1,9 @@
+package DungeonSource;
+
+public interface MonsterInterface extends DungeonCharacterInterface{
+
+  public void heal();
+  public void subtractHitPoints(int hitPoints);
+ 
+
+}//end Monster class
